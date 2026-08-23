07680e2e9dc608a1ebd3a034878cfe742a15d5e9
Lan xink <tranthilan882.nk@gmail.com>
2026-08-23 16:48:14 +0700

    update and add

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://wxtkadxcoqssaoustjga.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Pt8lW1WAzLwM8cLDy4TQtA_uv_CEwzA";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
